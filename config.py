a40df1ed177994bc2d26dab35db9eb4b0f1b686c
abhi16-1293 <rawatgit2@gmail.com>
2024-09-01 15:24:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("6526333457:AAGsjG4xx1HHcOdCFHPEcQTtfq8iUe8jApI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@Arawat22")
+OWNER_USERNAME=("Arawat22")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("6087454087")
