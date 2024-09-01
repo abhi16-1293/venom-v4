522994b97ee11f5a8302c355f2d75be5eed197a8
abhi16-1293 <rawatgit2@gmail.com>
2024-09-01 14:46:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("6526333457:AAGsjG4xx1HHcOdCFHPEcQTtfq8iUe8jApI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@Arawat22")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6087454087")
 
 
 
